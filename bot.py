701d96444f377725e2ebcbfdb509dd702f36a2d6
varun <74466129+nairvarun@users.noreply.github.com>
2021-10-25 19:33:16 +0530

    .

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 # todo:
-	# check if generated inital state is solvable (https://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/)
 	# add proper ai part
 	# add readme
 	# find good license and add it
