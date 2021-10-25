22bf827610ee8e2286139a985c0d0a5c3fdebf6a
varun <74466129+nairvarun@users.noreply.github.com>
2021-10-25 17:52:52 +0530

    added random and heuristic func

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ def print_board(board):
 	''' % (board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8], board[9])
 	print(b)
 
+
 # returns list of valid moves.
 def get_valid_moves(board, empty_pos):
 	if board[empty_pos] == board[1]:
@@ -44,36 +45,72 @@ def get_valid_moves(board, empty_pos):
 		print('invalid move')
 		print_board(board)
 
-# if entered move is valid, makes move.
-def make_move(board):
-	empty_pos = list(board.keys())[list(board.values()).index(' ')]
+
+# returns a random move for the ai.
+def get_ai_move_random(board, empty_pos):
+	print_board(board)
+	valid_moves = get_valid_moves(board, empty_pos)
+	best_move = random.choice(valid_moves)
+	return best_move
+
+
+# returns the best move it finds using just a simple huristic function for the ai.
+def get_ai_move_heuristic_func(board, goal_state, empty_pos):
+	print_board(board)
+
+	valid_moves = get_valid_moves(board, empty_pos)
+	
+	best_h = 0
+	best_move = random.choice(valid_moves)
+
+	for move in valid_moves:
+		temp_board = board.copy()
+		make_move(move, temp_board, empty_pos)
+		# print_board(temp_board)
+		h = 0
+		# print('move:', move)
+		for i in temp_board:
+			if temp_board[i] == goal_state[i]:
+				h += 1
+		# print('h:', h)
+		# print('best_h:', best_h)
+		if h > best_h:
+			best_h = h
+			best_move = move
+		# print('best_h:', best_h)
+		# print('best_move:', best_move)
+
+	# print('final best_h:', best_h)
+	# print('final best_move:', best_move)
+	return best_move
+
+
+# returns the move that the human player wants to make if it is a valid move.
+def get_human_move(board, empty_pos):
+	print_board(board)
 	valid_moves = get_valid_moves(board, empty_pos)
 
-	print_board(board)
 	move = input('w = up \ns = down \na = left \nd = right \nyour move: ')
 
+	if 	move in valid_moves:
+		return move
+	else:
+		return None
+
+
+# makes move.
+def make_move(move, board, empty_pos):
 	if move == 'w':
-		if move in valid_moves:
-			board[empty_pos], board[empty_pos-3] = board[empty_pos-3], board[empty_pos]
-		else:
-			print("invalid move")
+		board[empty_pos], board[empty_pos-3] = board[empty_pos-3], board[empty_pos]
 	elif move == 's':
-		if move in valid_moves:
-			board[empty_pos], board[empty_pos+3] = board[empty_pos+3], board[empty_pos]
-		else:
-			print("invalid move")
+		board[empty_pos], board[empty_pos+3] = board[empty_pos+3], board[empty_pos]
 	elif move == 'a':
-		if move in valid_moves:
-			board[empty_pos], board[empty_pos-1] = board[empty_pos-1], board[empty_pos]
-		else:
-			print("invalid move")
+		board[empty_pos], board[empty_pos-1] = board[empty_pos-1], board[empty_pos]
 	elif move == 'd':
-		if move in valid_moves:
-			board[empty_pos], board[empty_pos+1] = board[empty_pos+1], board[empty_pos]
-		else:
-			print("invalid move")
+		board[empty_pos], board[empty_pos+1] = board[empty_pos+1], board[empty_pos]
 	else:
 		print("invalid move")
+
 
 # game loop
 def start_game(board):
@@ -92,24 +129,47 @@ def start_game(board):
 	print('========================')
 	print('initial state:')
 	print_board(board)
+	# print(board)
 	print('goal state:')
 	print_board(goal_state)
+	# print(goal_state)
 	print('========================')
 
 	# board = {
-	# 1: 1,
-	# 2: 2,
-	# 3: 3,
-	# 4: 4,
-	# 5: 5,
-	# 6: 6,
-	# 7: 7,
-	# 8: ' ',
-	# 9: 8
+	# 	1: 1,
+	# 	2: 2,
+	# 	3: 3,
+	# 	4: 4,
+	# 	5: 5,
+	# 	6: 6,
+	# 	7: 7,
+	# 	8: ' ',
+	# 	9: 8
 	# }
 
 	while board != goal_state:
-		make_move(board)
+
+		empty_pos = list(board.keys())[list(board.values()).index(' ')]
+
+		# region for humans: 
+
+		move = get_human_move(board, empty_pos)
+		while move == None:
+			move = get_human_move(board, empty_pos)
+
+		# endregion
+		
+		# region for ai:
+
+		# random move
+		# move = get_ai_move_random(board, empty_pos)
+		
+		# unsing only heuristic function 
+		# move = get_ai_move_heuristic_func(board, goal_state, empty_pos)
+
+		# endregion
+
+		make_move(move, board, empty_pos)
 
 	print_board(board)
 	print('CONGRATULATIONS!!! you won!!')
